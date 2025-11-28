8e6189921c0edba01c67e97b7d213914ad6d2ce5
NSMG27 <nicolas.mosquerago@outlook.com>
2025-11-28 10:58:40 -0500

    Cambios 28/11/2025

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,27 +2,27 @@ import { Routes } from '@angular/router';
 import { authGuard } from './Core/Guards/Auth/auth-guard';
 
 export const routes: Routes = [
-     /*{
-            path: '',
-            loadComponent: () => import('./index/index.component').then((m) => m.IndexComponent)
-        },*/
-        {
-            path: 'login', 
-            loadChildren: () => import('./login/routes')
-        },
-        {
-            path: 'dashboard',
-            title: 'Dashboard | Mini Instagram',
-            loadChildren: () => import('./dashboard/routes'), 
-            canActivate: [authGuard]
-        },
-        {
-            path: 'register', 
-            loadChildren: () => import('./register/routes')
-        },
-        {
-            path: '**',
-            loadComponent: () =>
-                import('./page-not-found/page-not-found').then((m) => m.PageNotFound),
-        },
+    {
+        path: '', 
+        loadChildren: () => import('./login/routes')
+    },
+    {
+        path: 'login', 
+        loadChildren: () => import('./login/routes')
+    },
+    {
+        path: 'dashboard',
+        title: 'Dashboard | Mini Instagram',
+        loadChildren: () => import('./dashboard/routes'), 
+        canActivate: [authGuard]
+    },
+    {
+        path: 'register', 
+        loadChildren: () => import('./register/routes')
+    },
+    {
+        path: '**',
+        loadComponent: () =>
+            import('./page-not-found/page-not-found').then((m) => m.PageNotFound),
+    }
 ];
